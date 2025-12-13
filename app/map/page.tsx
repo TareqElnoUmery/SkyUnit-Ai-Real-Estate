7b14f20ee898dc0b91027668eff34cd3be95aecc
TareqElnoUmery <tareqtareq20222@gmail.com>
2025-12-13 10:09:26 +0200

    Create page.tsx
    Signed-off-by: TareqElnoUmery <tareqtareq20222@gmail.com>

---
+++ app/map/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function MapPage() {
+  return (
+    <div dir="rtl" style={{background: '#0a0e27', color: '#e8f4f8', minHeight: '100vh', padding: '2rem 0'}}>
+      <div style={{maxWidth: '1200px', margin: '0 auto', padding: '0 2rem'}}>
+        <h1 style={{fontSize: '2.5rem', marginBottom: '1rem', color: '#4dbfe8'}}الخريطة</h1>
+        <p>Map of projects coming soon...</p>
+      </div>
+    </div>
+  );
+}
